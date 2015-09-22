8866310d372cbfbc5a0d0312fa735f1f9cac6034
Agenric <agenric@foxmail.com>
2015-09-22 16:13:47 +0800

    Initial Commit

---
+++ AGTableViewCell/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AGTableViewCell
+//
+//  Created by Agenric on 15/9/22.
+//  Copyright (c) 2015年 Agenric. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AGTableViewCell/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AGTableViewCell
+//
+//  Created by Agenric on 15/9/22.
+//  Copyright (c) 2015年 Agenric. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
